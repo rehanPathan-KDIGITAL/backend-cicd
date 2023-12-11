f1ba6df9b8720d4569fd07272c67f0b0230b11c1
Rehan Pathan <rehan.pathan@kdigitalcurry.com>
2023-12-11 11:14:56 +0530

    backend

--- app.js
+++ app.js
@@ -2,7 +2,9 @@ import express from "express";
 const app = express();
 
 app.get("/", (req, res) => {
-    return res.status(200).json({ msg: "Hello World" });
+  return res.status(200).json({ msg: "Hello World" });
 });
 
-app.listen(3000);
+app.listen(3000, () => {
+  console.log("server started");
+});
